5d151cb0e62124accf3d9108388028bd37387bbe
maunikthakkar <maunik2119@gmail.com>
2023-12-19 15:58:16 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import "./App.css";
+import React,{useState} from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+const App =()=>{
+ const pageSize = 20;
+const  apiKey = process.env.REACT_APP_MY_API
+  const [progress, setProgress] = useState(0);
+    return (
+      <BrowserRouter>
+        <Navbar />
+        <LoadingBar color="#f11946" progress={progress} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="general"
+                pageSize={pageSize}
+                country="in"
+                category="general"
+              />
+            }
+          />
+          <Route
+            path="/business"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="business"
+                pageSize={pageSize}
+                country="in"
+                category="business"
+              />
+            }
+          />
+          <Route
+            path="/entertainment"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="entertainment"
+                pageSize={pageSize}
+                country="in"
+                category="entertainment"
+              />
+            }
+          />
+          <Route
+            path="/general"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="general"
+                pageSize={pageSize}
+                country="in"
+                category="general"
+              />
+            }
+          />
+          <Route
+            path="/health"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="health"
+                pageSize={pageSize}
+                country="in"
+                category="health"
+              />
+            }
+          />
+          <Route
+            path="/science"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="science"
+                pageSize={pageSize}
+                country="in"
+                category="science"
+              />
+            }
+          />
+          <Route
+            path="/sports"  
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="sports"
+                pageSize={pageSize}
+                country="in"
+                category="sports"
+              />
+            }
+          />
+          <Route
+            path="/technology"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="technology"
+                pageSize={pageSize}
+                country="in"
+                category="technology"
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    );
+  
+}
+export default App;
